df10661e09774dd04ca97019b61667700d1db124
Shanlin Li <sli154@stevens.edu>
2024-12-07 20:41:29 -0500

    Update pygame project.py

--- pygame project.py
+++ pygame project.py
@@ -1,3 +1,24 @@
 # Author: Wenyi Gong, Jiakai Hu, Shanlin Li 
 # Date: 12/7/2024  
 #Description: Using pygame to build a battle game.
+
+import pygame
+import sys
+
+# Initialize pygame
+pygame.init()
+
+# Define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREY = (200, 200, 200)
+RED = (255, 0, 0)
+BLUE = (128, 255, 255)
+GREEN = (0, 255, 0)
+YELLOW = (255, 255, 128)
+DARK_GREEN = (0, 150, 0)
+
+# Default display size
+WIDTH, HEIGHT = 1200, 800  # Slightly larger size to fit all elements
+screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+pygame.display.set_caption("Game Settings")
