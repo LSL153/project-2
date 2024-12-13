d75c276bdf60ecbec3a147eeddf4bd0c31ac41a3
Shanlin Li <sli154@stevens.edu>
2024-12-13 18:33:39 -0500

    Update phaseBattleUI.py

--- phaseBattleUI.py
+++ phaseBattleUI.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
+# Author: Wenyi Gong, Jiakai Hu, Shanlin Li 
+# Date: 12/7/2024  
+#Description: Using pygame to build a battle game.
 
 import threading
 import time
